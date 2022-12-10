32cfd36f269f53a24a87347b9f1de602b3c34724
Muhaimen Khatib <khatibmuhaimen@gmail.com>
2022-12-10 06:35:25 -0800

    convert it to AAA Patern

--- main.test.js
+++ main.test.js
@@ -1,36 +1,86 @@
 const Add = require('./main')
 
 test("addition of none given arguments" , () => {
-    expect(Add()).toBe(0)
+    //Arrange
+    const arg = null;
+    //Act
+    const result = Add();
+    //Assert
+    expect(result).toBe(0)
 });
 test("addition of empty equal zero" , () => {
-    expect(Add("")).toBe(0)
+    //Arrange
+    const arg = "";
+    //Act
+    const result = Add(arg);
+    //Assert
+    expect(result).toBe(0)
 });
 test("addition of empty equal zero" , () => {
-    expect(Add(", ")).toBe(0)
+    //Arrange
+    const arg = ", ";
+    //Act
+    const result = Add(arg);
+    //Assert
+    expect(result).toBe(0)
 });
 
 test("addition of 77 equal 77" , () => {
-    expect(Add("77")).toBe(77)
+    //Arrange
+    const arg = "77";
+    //Act
+    const result = Add(arg);
+    //Assert
+    expect(result).toBe(77)
 });
 
 test("addition of 55 and 88 equals 143",()=>{
-    expect(Add("55,88")).toBe(143)
+    //Arrange
+    const arg = "55,88";
+    //Act
+    const result = Add(arg);
+    //Assert
+    expect(result).toBe(143)
 })
 test("Addition of more than 2 items in the given string" , () => {
-    expect(()=>Add("1,2,3")).toThrow('Exceeded Numbers')
+    //Arrange
+    const arg = "1,2,3";
+    //Act
+    const result = ()=>Add(arg);
+    //Assert
+    expect(result).toThrow('Exceeded Numbers')
 })
 
-test("Addition of strings contains incorrect numbers" , () => {
-    expect(()=>Add(", ,")).toThrow('Exceeded Numbers')
+test("Addition of strings contains empty  numbers" , () => {
+    //Arrange
+    const arg = ", ,";
+    //Act
+    const result = ()=>Add(arg);
+    //Assert
+    expect(result).toThrow('Exceeded Numbers')
 })
-test("Addition of strings contains empty numbers" , () => {
-    expect(()=>Add("1,dsf")).toThrow('Wrong Numbers')
+test("Addition of strings contains  incorrect numbers" , () => {
+    //Arrange
+    const arg = "1,dsf";
+    //Act
+    const result = ()=>Add(arg);
+    //Assert
+    expect(result).toThrow('Wrong Numbers')
 })
 test("if there is negative numbers" , ()=> {
+    //Arrange
+    const arg = "1,dsf";
+    //Act
+    const result = ()=>Add(arg);
+    //Assert
     expect(()=>Add("1,-1")).toThrow('Negatives Not Allowed')
 })
 test("if there is  number larger than 1000" , ()=> {
-    expect(Add("187,1005")).toBe(187)
+    //Arrange
+    const arg = "187,1005";
+    //Act
+    const result = Add(arg)
+    //Assert
+    expect(result).toBe(187)
 })
 
